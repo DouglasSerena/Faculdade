bbc3bff0ea014f14ed2acd9ea517de5c466a7a8b
DouglasSerena <douglas222serena@rede.ulbra.br>
2021-05-23 17:41:14 -0300

    feat: Merge sort com javascript e typescript

---
+++ Estrutura_de_Dados/2021-05-20/Merge_Sort/javascript/main.js
@@ -0,0 +1,9 @@
+const { mergeSort } = require('./mergeSort');
+
+const array = [7, 15, 19, 21, 5, 9, 11, 90];
+
+console.log('Array antes: ' + array.join(','));
+
+mergeSort(array);
+
+console.log('Array depois: ' + array.join(','));
---
+++ Estrutura_de_Dados/2021-05-20/Merge_Sort/javascript/mergeSort.js
@@ -0,0 +1,44 @@
+function mergeSort(
+  array,
+  left = undefined,
+  middle = undefined,
+  right = undefined
+) {
+  left = left || 0;
+  middle = middle || array.length / 2;
+  right = right || array.length;
+
+  // transfere os elementos entre left e right para um array auxiliar.
+  let helper = [...array];
+
+  let index = left;
+  let indexJ = middle;
+  let indexK = left;
+
+  while (index <= middle && indexJ <= right) {
+    if (helper[index] < helper[indexJ]) {
+      array[indexK] = helper[index];
+      index++;
+    } else {
+      array[indexK] = helper[indexJ];
+      indexJ++;
+    }
+    indexK++;
+  }
+
+  // se a metade inicial não foi toda consumida, faz o append.
+  while (index <= middle) {
+    array[indexK] = helper[index];
+    index++;
+    indexK++;
+  }
+
+  // se a metade final não foi toda consumida, faz o append.
+  while (indexJ <= right) {
+    array[indexK] = helper[indexJ];
+    indexJ++;
+    indexK++;
+  }
+}
+
+module.exports.mergeSort = mergeSort;
